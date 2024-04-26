c89a7ee1b83274df5933630518c27b0e376aef5b
jadverderada <jenelleadverderada@Jenelles-MacBook-Air.local>
2024-04-26 12:26:55 -0700

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -31,8 +31,10 @@ public class FamilyTree
         
         void addChild(TreeNode childNode)
         {
-            // Add childNode to this node's children list. Also
+        	// Add childNode to this node's children list. Also
             // set childNode's parent to this node.
+        	children.add(childNode);
+        	childNode.parent = this;
         }
         
         
@@ -41,16 +43,21 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (targetName.equals(parent.getName())) {
                 return this;
+            }
+            else {
                     
             // No, recurse. Check all children of this node.
-            for (TreeNode child: children)
-            {
+           		for (TreeNode child: children)
+           		{
+           			if(child.getNodeWithName(targetName)!=null){
+            		return this;
+            	}
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+           		}
             }
-            
             // Not found anywhere.
             return null;
         }
@@ -61,7 +68,11 @@ public class FamilyTree
         ArrayList<TreeNode> collectAncestorsToList()
         {
             ArrayList<TreeNode> ancestors = new ArrayList<>();
-
+            TreeNode node = this;
+            while(node.parent!=null) {
+            	ancestors.add(node.parent);
+            	node=node.parent;
+            }
             // ?????  Collect ancestors of this TreeNode into the array list. HINT: going up
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
